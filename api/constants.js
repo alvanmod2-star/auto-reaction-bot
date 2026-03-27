3d21b09e1cca38fca46486b6fdc38e9bfaa0e0a6
alvanmod2-star <alvanmod2@gmail.com>
2026-03-27 12:28:13 +0300

    Update constants.js

--- api/constants.js
+++ api/constants.js
@@ -1,30 +1,15 @@
-export const startMessage = `👋  اهلا حياتي , UserName !
+export const startMessage = `👋 اهلا حياتي، UserName !
 
-مرحباً بك في بوت التفاعل التلقائي للرموز التعبيرية 🎉، جاهز لإضفاء المزيد من البهجة على محادثاتك!
+مرحباً بك في بوت التفاعل التلقائي 🎉
 
-💁‍♂️ إليك كيف أُضفي لمسة مميزة على محادثاتك:
+💁‍♂️ للحصول على مساعده قم بمحادثتي: /donate`
 
-*✨      للحصول على مساعده قم بمحادثتي   : /donate`
-
-export const donateMessage = `  قم بمراسلتي هناا @mu_312   ! 🌟🚀`
+export const donateMessage = `قم بمراسلتي هناا @mu_312 ! 🌟🚀`
 
 export const htmlContent = `
 <!DOCTYPE html>
-<html lang="en">
-<head>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Telegram Auto Reaction Bot</title>
-<meta name="description" content="Single Developers presents an open-source Telegram Auto Reaction bot developed by Malith-Rukshan. Reacts to all messages received from chats automatically. Serverless deployment on Cloudflare.">
-<meta name="keywords" content="Telegram bot, auto reaction bot, open-source, Single Developers, Malith-Rukshan, Telegram auto reaction, serverless telegram bot, reaction automation">
-<script async defer src="https://buttons.github.io/buttons.js"></script>
-<style>
-  body, html {
-    height: 100%; margin: 0; display: flex; justify-content: center; align-items: center; flex-direction: column; font-family: Arial, sans-serif;
-  }
-  .logo {
-    width: 60%; margin-bottom: 20px;
-  }
-  .title { margin-bottom: 20px; font-size: 34px; font-weight: bold; color: #333; text-align: center; }
-  .button {
-    padding: 10px 20px; margin: 10px; font-size: 16px; cursor: pointer; text-align: center; color: #fff; border: none; border-radius: 15px
+<html>
+<head><title>Bot Live</title></head>
+<body><h1>البوت شغال بنجاح! ✅</h1></body>
+</html>
+`;
